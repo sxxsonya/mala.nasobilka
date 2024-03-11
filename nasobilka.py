c4cb981a36dba53e5874a35747e0289e9fcd23c2
Sofiia Shokalenko <sofiashokalenko@gmail.com>
2024-03-11 09:31:55 +0100

    fghn

--- nasobilka.py
+++ nasobilka.py
@@ -1,5 +1,13 @@
 import random  
 
+Pocetbodu = 0
+
+plusbody = 1
+
+def PocetB (Pocetbodu, plusbody):
+    PocetboduF = Pocetbodu + plusbody
+    return PocetboduF
+    
 def nasobeni(a,b):
     vysledek = a * b
     return vysledek
@@ -9,13 +17,18 @@ def vyhodnoceni(vysledek, porovnani):
     if vysledek == porovnani:
         print("ty jsi ale šikulka, mas to spravne")
         odpoved = True
+        PocetboduF = PocetB(Pocetbodu, plusbody)
+        Pocetbodu = PocetboduF
+        print ("Mate", PocetboduF, "bodu")
     else:
         print("achjoo")
     return odpoved
     
 for x in range(9):
+
     a = random.randint(1,10)
     b = random.randint(1,10)
+
     porovnani = input(f"{a} * {b} = ")
     porovnani = int(porovnani)
     vysledek = nasobeni(a, b)
